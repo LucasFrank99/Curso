232a220c5c7caec3de0f7b2d3d6ebd67f7c3384c
Lucas Frank <140353980+LucasFrank99@users.noreply.github.com>
2024-08-16 16:35:41 -0300

    Add files via upload

---
+++ Nova pasta (2)/teste.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplica(x, y) {
+    return x * y;
+}
+const resultado = multiplica(10, 3);
+console.log(resultado);
+function dizOi(nome) {
+    return "Olá " + nome;
+}
+const mesnsagem = dizOi("Lucas");
+console.log(mesnsagem);
---
+++ Nova pasta (2)/teste.ts
@@ -0,0 +1,13 @@
+function multiplica(x: number, y: number): number {
+    return x * y;
+}
+
+const resultado = multiplica(10, 3);
+console.log(resultado);
+
+function dizOi(nome: string): string {
+    return "Olá " + nome;
+}
+
+const mesnsagem = dizOi("Lucas");
+console.log(mesnsagem);
